8a61c23eb1484ff89efa905070892c17a52adbd2
nazadub18 <nazadub18@gmail.com>
2026-02-14 12:24:50 -0600

    Commit 2: Agregué información del curso

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+// COMMIT 1: Agrego datos personales
+// NOMBRE: NAZARETH BELÉN DUBÓN JUÁREZ
+// NÚMERO DE CUENTA: 20221000123
+Console.WriteLine("EjercicioGit");
+
+//COMMIT 2: información del curso
+//Clase: Programación Orientada a objetos
+//Fecha de entrega: Domingo 15 de Febrero del 2026
+Console.WriteLine("Primer Commit Realizado...");
+
